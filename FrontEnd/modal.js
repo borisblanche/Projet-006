baa5963de73ab1cc539221f33c188b79235a7cad
boris blanche <borisblanche77gmail.com>
2023-09-18 10:19:13 +0200

    commit 3

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -211,8 +211,10 @@ async function openModalAjoutPhoto() {
     imageInput.click();
   });
 
-  const svgCode = `<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6h96 32H424c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/></svg>`;
+  const svgCode = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#cbd6dc}</style><path d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/></svg>`;
   const svgDataUrl = `data:image/svg+xml;base64,${btoa(svgCode)}`;
+  
+
 
   const imagePreview = document.createElement("img");
   imagePreview.id = "imagePreview";
@@ -225,6 +227,7 @@ async function openModalAjoutPhoto() {
   imagePreview.classList.add("custom-background");
   imagePreview.style.width = "76px";
   imagePreview.style.minHeight = "76px";
+  
 
   imageInput.addEventListener("change", (event) => {
     const selectedImage = event.target.files[0];
